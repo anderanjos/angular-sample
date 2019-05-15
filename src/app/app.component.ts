07537952b779ec69c96c53013389959b88e48353
Anderson dos Anjos <anderson.asilva@lasa.com.br>
2019-05-15 08:05:35 -0300

    Adicionando comentarios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,15 +11,20 @@ export class AppComponent {
   heroes: Object;
   constructor(private _service: AppService) { }
 
+  // metodo do ciclo de vida que sera executado assim que a aplicacao subir
   ngOnInit() {
     this.init()
   }
 
+
+  /**
+   * chama o loadHeroes e coloca o conteudo na variavel "heroes" que sera exibida no front (pagina de teste)
+   */
   init() {
     this._service.loadHeroes()
       .subscribe(res => {
-        this.heroes = res.data['results']
-        console.log(`API Results: \n${JSON.stringify(this.heroes, null, 2)}`)
+        this.heroes = res.data['results'];
+        console.log(`Resultado retornado pela api (para debug no console do browser): \n${JSON.stringify(this.heroes, null, 2)}`)
       })
   }
 
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -10,7 +10,12 @@ export class AppService {
 
   constructor(private http: HttpClient) { }
 
-  loadHeroes(): Observable<IHero>{
+  /**
+   * funcao que consulta a api e retorna o resultado em conformidade com a interface definida (nesse caso IHero)
+   * A inteface vai depender do retorno da sua api. 
+   * No caso desse exemplo, a api retorna uma propriedade "data" no primeiro nivel do payload 
+  */
+  loadHeroes(): Observable<IHero> {
     return this.http.get<IHero>('https://gateway.marvel.com:443/v1/public/events/269/characters?ts=1&apikey=30725f88857cb0a80685b8ecb39e408e&hash=ab55f23f2bed95b96528a0eeef8705db')
   }
 }
